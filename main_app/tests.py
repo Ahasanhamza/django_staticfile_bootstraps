59eb2359817f68d0b32c18d1a0e308e851b6a455
Ahasanhamza <40607240+Ahasanhamza@users.noreply.github.com>
2021-09-14 03:26:11 +0600

    Add files via upload

---
+++ main_app/tests.py
@@ -0,0 +1,9 @@
+from django.test import SimpleTestCase
+class MainAppTest(SimpleTestCase):
+    def test_home_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code,200)
+    def test_about_page(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code,200)
+
